4ad312d6a5790cefa86c151f17bd5af777db7341
Bartek <bartlomiej.pawel.borzyszkowski@intel.com>
2019-07-27 15:35:10 +0200

    quantum model

--- quantum_model.py
+++ quantum_model.py
@@ -4,15 +4,18 @@ import tensorflow as tf
 from tensorflow import keras
 
 
+json_file = 'rbm.json'
+
 input1 = keras.layers.Input(shape=(74,))
 x1 = keras.layers.Dense(74, activation='relu')(input1)
-# x1 = tf.keras.backend.transpose(x1)
-output = keras.layers.Dense(1, activation='relu')(x1)
 
-model = keras.models.Model(inputs=[input1], outputs=output)
+model = keras.models.Model(inputs=[input1], outputs=x1)
 model.compile(optimizer=tf.train.AdamOptimizer(),
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
+model.summary()
 
+# model.load_weights(json_file)
 # model.fit(train_images, train_labels, epochs=5)
+# model = model_from_json(json_file)
 
